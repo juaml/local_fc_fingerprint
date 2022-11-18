0d1837460cab68efda2840501745fb4debfa9923
Kaustubh Patil <kaustubh.patil@gmail.com>
2022-11-18 14:29:51 +0100

    setup file that gets the dataset and spm12

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# get the datalad dataset
+datalad clone https://github.com/OpenNeuroDerivatives/ds000115-fmriprep.git data
+
+# get spm12
+git clone https://github.com/spm/spm12.git spm12
+
+
